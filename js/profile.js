dbbb6b241943153fbe046998fac6b6013572ce96
mihaly81 <gacsomihaly1@gmail.com>
2025-04-03 12:41:20 +0200

    home.html javitás

--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const btnSave = document.getElementById('btnSave');
-
+    const iconUser = document.getElementById('usericon'); // Módosítottam, hogy ID alapján keresse
 
     if (btnSave) {
         btnSave.addEventListener('click', save);
